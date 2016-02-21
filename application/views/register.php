8211f0b56ef1b82336cd06c9ae0da41e12807551
Bertie Wheen <wheen.b@gmail.com>
2016-02-21 11:46:06 +0000

    Revert idiocy

--- application/views/register.php
+++ application/views/register.php
@@ -3,9 +3,9 @@
 <head>
 <meta charset="utf-8">
 <title></title>
-<link href="http://trip-scanner.net/assets/style.css" rel="stylesheet" type="text/css">
+<link href="<?php echo base_url('assets/style.css'); ?>" rel="stylesheet" type="text/css">
 </head>
-<body background="http://trip-scanner.net/assets/Wallpapers/wall2.jpg">
+<body background="<?php echo base_url('assets/Wallpapers/wall2.jpg'); ?>">
 <h1>TRIP SCANNER</h1>
 <section class="loginform cf">
 
@@ -13,7 +13,7 @@
 <?php if(!empty($this -> session -> userdata('error_message'))) : ?>
 	<div class="error"><?php echo $this -> session -> userdata('error_message'); $this -> session -> set_userdata('error_message', '') ; ?></div>
 <?php endif; ?>
-<form class="loginform" name="login" action="http://trip-scanner.net/authentification/register" method="post" accept-charset="utf-8">
+<form class="loginform" name="login" action="<?php echo base_url('authentification/register'); ?>" method="post" accept-charset="utf-8">
 	  <input type="text" name="name" placeholder="Full Name"/>
 	  <input type="text" name="email" placeholder="email address"/>
       <input type="password" name="password" placeholder="password" />
